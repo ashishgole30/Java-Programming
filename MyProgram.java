be857904088b6cb1156c0cbd27f6dc40ab8fa2df
ashishgole30 <ashish.gole3010@gmail.com>
2026-04-01 01:10:24 +0530

    m

--- MyProgram.java
+++ MyProgram.java
@@ -1,6 +1,37 @@
 public class MyProgram {
     public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("-----------------------------");
 
+        int num1 = 2;
+        int num2 = 3;
+        int result = num1 + num2;
+        System.out.println(result);
+
+        System.out.println("-----------------------------");
+        int num3 = 1232;
+        byte num4 = 127;
+        short num5 = 558;
+        long num6 = 242l;
+        float f = 5.8f;
+        double d = 5.8;
+        char c = 'k';
+        boolean b = true;
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println("-----------------------------");
+
+        int num7 = 100_000_000;
+        System.out.println(num7);
+        char character = 'a';
+        character++;
+        System.out.println(character);
+        System.out.println("-----------------------------");
     }
-
 }
