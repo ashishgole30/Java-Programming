da1f3657530e63f5b85cfcefd4154f1f8620acaf
ashishgole30 <ashish.gole3010@gmail.com>
2026-04-02 00:10:31 +0530

    m

---
+++ ProgramSeven.java
@@ -0,0 +1,13 @@
+public class ProgramSeven {
+    public static void main(String[] args) {
+        // do while
+
+        int i = 5;
+        do {
+            System.out.println("Hi " + i);
+            i++;
+        } while (i <= 4);
+
+        System.out.println("---------------------");
+    }
+}
