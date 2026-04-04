f4d22dde3c60d1d9c5821b4c73d6438447c69776
ashishgole30 <ashish.gole3010@gmail.com>
2026-04-04 16:30:02 +0530

    m

--- Core Java/OOP Java/ProgramThree.java
+++ Core Java/OOP Java/ProgramThree.java
@@ -1,8 +1,12 @@
 // METHOD OVERLOADING
-
 class Calculator {
+
+    int num;// instance variable
+
     public int add(int n1, int n2, int n3) {
         return n1 + n2 + n3;
+
+        // n1 n2 n3 a re local variables
     }
 
     public int add(int n1, int n2) {
