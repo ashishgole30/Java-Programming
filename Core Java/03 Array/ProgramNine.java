6c9bd31a541b93cc4c858b687fba96509034ffab
ashishgole30 <ashish.gole3010@gmail.com>
2026-04-08 00:04:35 +0530

    m

--- Core Java/03 Array/ProgramNine.java
+++ Core Java/03 Array/ProgramNine.java
@@ -1,5 +1,15 @@
 public class ProgramNine {
     public static void main(String[] args) {
 
+        int nums[] = new int[4];
+        nums[0] = 4;
+        nums[1] = 8;
+        nums[2] = 3;
+        nums[3] = 9;
+
+        for (int n : nums) {
+            System.out.println(n);
+        }
+
     }
 }
