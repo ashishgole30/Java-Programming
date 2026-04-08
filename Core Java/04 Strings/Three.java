d1ff5ceec375c70c294b34b0c8a85807d1b89355
ashishgole30 <ashish.gole3010@gmail.com>
2026-04-08 15:23:17 +0530

    m

---
+++ Core Java/04 Strings/Three.java
@@ -0,0 +1,12 @@
+public class Three {
+    public static void main(String[] args) {
+        // 2. String Builder
+
+        // What is the difference between String buffer and String builder
+        // There is only one difference
+
+        // String Buffer Is Thread Safe
+        // String Builder Is NOT Thread Safe
+
+    }
+}
