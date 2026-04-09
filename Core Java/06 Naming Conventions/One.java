c44fbd610b5a3e34b53ec0c26111aeb793afdf0d
ashishgole30 <ashish.gole3010@gmail.com>
2026-04-10 00:55:08 +0530

    m

---
+++ Core Java/06 Naming Conventions/One.java
@@ -0,0 +1,16 @@
+public class One {
+    public static void main(String[] args) {
+
+        // Camel casing
+
+        // class and interfaces - Capital letter ex. Calculator
+        // variables and methods - small letter ex. marks , show()
+        // constants - ALL CAPITAL ex. PIE
+
+        // showMyMarks()
+        // MyData
+
+        // age, DATA , Human()
+
+    }
+}
