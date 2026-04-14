bbb41945208fc3b6a59b5ee0efe0d87da3452c7b
ashishgole30 <ashish.gole3010@gmail.com>
2026-04-14 23:35:18 +0530

    m

--- Core Java/08 OOP Java/One.java
+++ Core Java/08 OOP Java/One.java
@@ -5,5 +5,18 @@ public class One {
 
         // IS , HAS
 
+        // parent class - child class
+        // Super class = sub class
+        // base class - derived class
+    }
+}
+
+class Calc {
+    public int add(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    public int sub(int n1, int n2) {
+        return n1 - n2;
     }
 }
