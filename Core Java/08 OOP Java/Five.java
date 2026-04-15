55a68b325d72bd3aec4806e3b682cf2760b4c6ac
ashishgole30 <ashish.gole3010@gmail.com>
2026-04-15 23:29:30 +0530

    m

---
+++ Core Java/08 OOP Java/Five.java
@@ -0,0 +1,27 @@
+public class Five {
+    public static void main(String[] args) {
+        // METHOD OVERRIDING
+
+        B obj = new B();
+        obj.show();
+        obj.config();
+    }
+}
+
+class A {
+    public void show() {
+        System.out.println("in A show");
+    }
+
+    public void config() {
+        System.out.println("in A Config");
+    }
+}
+
+class B extends A {
+
+    public void show() {
+        System.out.println("in B show");
+    }
+
+}
