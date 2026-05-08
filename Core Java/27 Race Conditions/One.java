3adf2541511a58ea11df4cd11a32b4906228a54a
ashishgole30 <ashish.gole3010@gmail.com>
2026-05-08 23:50:23 +0530

    m

--- Core Java/27 Race Conditions/One.java
+++ Core Java/27 Race Conditions/One.java
@@ -3,6 +3,16 @@ public class One {
         // RACE CONDITIONS
 
         // Mutations = change
+        // String are immutable
 
+        // What if you are doing mutation with multiple threads?
+        // lets say we have two threads , t1 , t2
+        // and they both change the value of i
+        // it will create instabaility in the program
+
+        // Threads and mutations dont go together
+        // thread safe-> only one thread can work with something at one point
+        // ex : if t1 is working with show() method , t2 cannot work with show() method
+        // at the same time
     }
 }
